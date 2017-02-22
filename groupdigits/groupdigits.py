aac2c5d22afa48c1688b97a28384e7583ea9f84c
Danver Braganza <danver@hipmunk.com>
2017-02-22 14:59:54 -0800

    recant prior malice and correct tests

--- groupdigits/groupdigits.py
+++ groupdigits/groupdigits.py
@@ -4,11 +4,10 @@
 
 def group_digits(amount, grouping, separator):
     """Formats an amount according to the grouping strategy in groupings. Groups
-    are separated with a `separator` character.
+    are separated with a `separator` string.
 
     Args:
         amount: Number - The amount to be formatted.
-
         grouping: List[int] - Each element is the number of digits in a group;
             elements with higher indices are further left. An element with value
             -1 means that no further grouping is done. An element with value 0
@@ -53,8 +52,6 @@ BASIC_TESTS = [
     (4154041062, [4, 3, 0], "-", "415-404-1062"),
     (4154041062, [4, 3, -1], "-", "415-404-1062"),
     (10, [1, 1, 1, 1, -1], "! ", "1! 0"),
-    (2000.3, [3, 0], " ", "2 000.3"),
-    (-12842.42, [3, 0], " ", "-12 842.42"),
     (56781234, [1, 0], "", "56781234"),
     (56781234, [-1], ".", "56781234"),
     (19216801, [1, 1, 3, 0], ".", "192.168.0.1"),
